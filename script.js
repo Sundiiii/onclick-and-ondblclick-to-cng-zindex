97c2d4319a4cdb14a323eca0a78d76957ad77bc9
Sundar  Deshmukh <109438264+Sundiiii@users.noreply.github.com>
2023-05-02 02:47:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let l1=document.getElementById('T');
+let d=document.getElementById('Tt');
+let btn=document.querySelector('button');
+  btn.addEventListener("click", chan=>{
+    d.style.zIndex=2;
+    l1.style.zIndex=1;
+      
+    });
+  btn.addEventListener("dblclick", chan1=>{
+     
+      l1.style.zIndex=5;
+    });
+    
+   
